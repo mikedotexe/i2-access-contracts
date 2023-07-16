f1fbef385483d41d0d3b18d17f9d2df4295ede49
Mike Purvis <mikedotexe@gmail.com>
2023-07-16 11:42:19 +0200

    add notes for future methods on access contract to add/remove keys, freeze, add croncat factory addr

--- packages/cw-access-contract-pkg/src/msgs/execute_msg.rs
+++ packages/cw-access-contract-pkg/src/msgs/execute_msg.rs
@@ -4,5 +4,14 @@ use cosmwasm_std::CosmosMsg;
 /// Lists the available execute messages for this contract
 #[cw_serde]
 pub enum ExecuteMsg {
+    /// Attempt to "authz execute" a CosmosMsg
+    /// Custom contract logic can entirely control whether this happens
     Exec { cosmos_msg: CosmosMsg },
+    /*
+        TODO: add methods
+            - add_caller
+            - remove_caller
+            - freeze
+            - update_croncat_factory
+    */
 }
